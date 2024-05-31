4ea9fc4cd5d87d38fb81486cdc8d420363c5a780
transformers512 <hamidmalikafu@gmail.com>
2024-05-31 18:41:33 +0700

    mengedit file bebek.py

--- bebek.py
+++ bebek.py
@@ -2,7 +2,7 @@ def pangkat(n):
   return lambda angka:angka**n
 
 hasil_pangkat = pangkat(3)
-print(f"hasil dari 5 pangkat 3 adalah: {hasil_pangkat(5)}")
+print(f"hasil dari 6 pangkat 3 adalah: {hasil_pangkat(6)}")
 
 
 
